2c0157cc57c111f6677691ab2fda4a8a115ed22f
ricardoyf <ricardoyf@gmail.com>
2026-08-18 12:56:41 +0200

    Añade funcionamiento offline para Exam

---
+++ exam/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='exam-v1';
+const FILES=['./','./index.html','./data.js','./manifest.webmanifest','./exam-icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
